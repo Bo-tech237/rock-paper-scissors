1d9578f82d6c2b42872e409297c1dab31d5d9c40
Ferdinand <bokallitechnology@gmail.com>
2022-04-07 17:30:30 +0100

    Add draw option in the if statements

--- script.js
+++ script.js
@@ -6,22 +6,23 @@
     for (let i = 0; i < wordSelection.length; i++) {
         result = Math.floor(Math.random()*3);
     }
+        console.log(result);
         return result;
  }
 
  function computerPlay() {
+        console.log(wordSelection[randomWordSelect()].toUpperCase());
         return wordSelection[randomWordSelect()].toUpperCase();
  }
- console.log(wordSelection[randomWordSelect()].toUpperCase());
 
 
  // Anonymous player
 
  function player1() {
      choice = prompt("Enter either ROCK, PAPER or SCISSORS: ");
+     console.log(choice.toUpperCase());
      return choice.toUpperCase();
  }
- console.log(choice.toUpperCase());
 
  // Play Round
 
@@ -57,22 +58,30 @@
  function game() {
      let countWin = 0;
      let countLose = 0;
+     let countDraw = 0;
      for (let i = 1; i <= 5; i++) {
          alert(playRound(player1(), computerPlay()));
-         console.log(playRound(player1(), computerPlay()));
          if (checkResults === "win") {
              countWin = countWin + i;
-         } else {
+         } else if (checkResults === "lose") {
              countLose = countLose + i;
+         } else {
+             countDraw = countDraw + i;
          }
      }
+     console.log("wins: " + countWin);
+     console.log("loses: " + countLose);
+     console.log("draws: " + countDraw);
 
      if (countWin > countLose) {
          alert("You are the CHAMPION!");
          console.log("You are a CHAMPION!");
-     } else {
+     } else if (countWin < countLose) {
          alert("sorry you are DISQUALIFIED!");
          console.log("sorry you are DISQUALIFIED!");
+     } else {
+         alert("DRAW GAME");
+         console.log("DRAW GAME");
      }
  }
  
