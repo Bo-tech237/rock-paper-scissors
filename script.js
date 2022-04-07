bb606c5997ff1ed0eba92d064687901d70df5451
Ferdinand <bokallitechnology@gmail.com>
2022-04-07 14:24:14 +0100

    Add the playRound function

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
  const wordSelection = ["ROCK", "PAPER", "SCISSORS"];
 
  function randomWordSelect() {
-     
+
     for (let i = 0; i < wordSelection.length; i++) {
         result = Math.floor(Math.random()*3);
     }
@@ -21,4 +21,25 @@
      return choice.toUpperCase();
  }
 
- console.log(player1());
+ // Play Round
+
+ function playRound(playerSelection, computerSelection) {
+
+    if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
+        return "You win! " + playerSelection + " beats " + computerSelection;  
+    } else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
+        return "You win! " + playerSelection + " beats " + computerSelection;
+    } else if (playerSelection === "SCISSORS" && computerSelection == "PAPER") {
+        return "You win! " + playerSelection + " beats " + computerSelection;
+    } else if (playerSelection === "ROCK" && computerSelection === "PAPER") {
+        return "You lose! " + computerSelection + " beats " + playerSelection;
+    } else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
+        return "You lose! " + computerSelection + " beats " + playerSelection;
+    } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
+        return "You lose! " + computerSelection + " beats " + playerSelection;
+    } else {
+        return "Draw game! " + playerSelection + " equals " + computerSelection;
+    }
+ }
+
+ console.log(playRound(player1(), computerPlay()));
